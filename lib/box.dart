21b88e53fe314acc23294302731dd6bb5d56c86e
wiisportsresort <ccs009013@gmail.com>
2020-02-22 12:49:16 -0500

    update with box2d

---
+++ lib/box.dart
@@ -0,0 +1,19 @@
+// import 'dart:ui';
+
+// import 'package:foo_bar/game.dart';
+
+// class Box {
+//   Box(this.game, double x, double y) {
+//     rectRect = Rect.fromLTWH(x, y, game.tileSize, game.tileSize);
+//   }
+//   MainGame game;
+//   Rect rectRect;
+//   Paint rectPaint;
+
+
+//   void render(Canvas c) {
+//     c.drawRect(rectRect, rectPaint);
+//   }
+
+//   void update(double t) {}
+// }
