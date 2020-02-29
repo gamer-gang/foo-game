020f0b017d9aa0ebf691192f837748568c27af8c
Caleb Chan <ccss009013@gmail.com>
2020-02-28 19:40:53 -0500

    change package name and display name

--- android/app/src/main/kotlin/dev/wiisportsresorts/games/monument_platformer/MainActivity.kt
+++ android/app/src/main/kotlin/dev/wiisportsresorts/games/monument_platformer/MainActivity.kt
@@ -1,4 +1,4 @@
-package dev.wiisportsresorts.foo_bar
+package dev.wiisportsresorts.games.monument_platformer
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
