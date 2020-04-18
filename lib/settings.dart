8c81742b0faaa32b85a392501941d95f139e30a3
wiisportsresort <devdoge1@gmail.com>
2020-04-18 00:45:41 -0400

    formatting, naming

--- lib/settings.dart
+++ lib/settings.dart
@@ -10,43 +10,43 @@ class SettingsPage extends StatefulWidget {
   _SettingsPageState createState() => _SettingsPageState();
 }
 
-Map<String, String> settingsText = {
-  "animations": "",
-  "sendUsageData": "",
-};
-
 class _SettingsPageState extends State<SettingsPage> {
-  void setPref(pref, callback) async {
+  Map<String, String> settingsText = {
+    "animations": "",
+    "sendUsageData": "",
+  };
+
+  Future<bool> togglePref(pref, [bool defaultValue]) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     bool next;
     try {
       next = !prefs.getBool(pref);
       // ignore: unused_catch_clause
     } on AssertionError catch (err) {
-      next = true;
+      next = defaultValue;
     }
 
-    prefs.setBool(pref, next);
+    await prefs.setBool(pref, next);
 
-    callback(next);
+    return next;
   }
 
   @override
   void initState() {
     super.initState();
-    setSetting(["animations", "sendUsageData"]);
+    setBooleanSettingText(["animations", "sendUsageData"]);
   }
 
-  void setSetting(dynamic val) async {
+  void setBooleanSettingText(dynamic val) async {
     if (val is List) {
       for (dynamic i in val) {
-        String next = ((await getPref(bool, pref: i) ?? false) ? "On" : "Off");
+        String next = ((await getPref(i, bool) ?? false) ? "On" : "Off");
         setState(() {
           settingsText.update(i, (value) => next);
         });
       }
     } else {
-      String next = ((await getPref(bool, pref: val) ?? false) ? "On" : "Off");
+      String next = ((await getPref(val, bool) ?? false) ? "On" : "Off");
       setState(() {
         settingsText.update(val, (value) => next);
       });
@@ -57,44 +57,44 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: darkBlue.withAlpha(220),
         elevation: 0,
         title: Text("Settings"),
       ),
       body: Container(
+        color: Color(0xff121212),
         child: Center(
-          child: Column(children: <Widget>[
+          child: Column(children: [
             Spacer(flex: 1),
-            Row(children: <Widget>[
+            Row(children: [
               Spacer(flex: 1),
               Text("Animations"),
               Spacer(flex: 1),
               RaisedButton(
                 onPressed: () {
-                  setPref("animations", (val) => setSetting("animations"));
+                  togglePref("animations").then((val) => setBooleanSettingText("animations"));
                 },
                 child: Text(settingsText["animations"] ?? "..."),
               ),
               Spacer(flex: 20),
             ]),
             Spacer(flex: 1),
-            Row(children: <Widget>[
+            Row(children: [
               Spacer(flex: 1),
               Text("Send anonymous usage data"),
               Spacer(flex: 1),
               RaisedButton(
                 onPressed: () {
-                  setPref(
-                      "sendUsageData", (val) => setSetting("sendUsageData"));
+                  togglePref("sendUsageData").then((val) => setBooleanSettingText("sendUsageData"));
                 },
                 child: Text(settingsText["sendUsageData"] ?? ""),
               ),
-              Spacer(flex: 20)
+              Spacer(flex: 20),
             ]),
-            Spacer(flex: 15)
+            Spacer(flex: 15),
           ]),
         ),
       ),
     );
   }
 }
-
