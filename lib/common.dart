55c25aa6ca0df31ca9a1eae8d70479273291ec9d
wiisportsresort <devdoge1@gmail.com>
2020-04-22 20:16:05 -0400

    renaming

--- lib/common.dart
+++ lib/common.dart
@@ -3,15 +3,56 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 extension PrecisionRounding on double {
+  /// Returns a new double rounded to a certain number of digits.
   double roundTo(int places) {
     double mod = Math.pow(10.0, places);
     return ((this * mod).round().toDouble() / mod);
   }
 }
 
-extension SingleValues on Offset {
+extension OffsetUtil on Offset {
+  /// Returns a modified offset with the provided `x`.
   Offset withX(double x) => Offset(x, this.dy);
+
+  /// Returns a modified offset with the provided `y`.
   Offset withY(double y) => Offset(this.dx, y);
+
+  /// Scale only the `x` of the offset.
+  Offset scaleX(double x) => Offset(this.dx * x, this.dy);
+
+  /// Scale only the `y` of the offset.
+  Offset scaleY(double y) => Offset(this.dx, this.dy * y);
+
+  /// Translate only the `x` of the offset.
+  Offset translateX(double x) => Offset(this.dx + x, this.dy);
+
+  /// Translate only the `y` of the offset.
+  Offset translateY(double y) => Offset(this.dx, this.dy + y);
+
+  /// Returns a new offset scaled by the argument.
+  Offset scaleBy(Offset scale) =>
+      Offset(this.dx * scale.dx, this.dy * scale.dy);
+
+  /// Limit the offset to the given bounds.
+  Offset limit(Offset max) => Offset(
+        this.dx > max.dx ? max.dx : this.dx,
+        this.dy > max.dy ? max.dx : this.dy,
+      );
+
+  /// Limit the `x` component.
+  Offset limitX(double max) => Offset(
+        this.dx > max ? max : this.dx,
+        this.dy,
+      );
+
+  /// Generate a random offset, where each component is a random double in (-1.0, 1.0), exclusive.
+  static Offset random() {
+    Math.Random r = Math.Random();
+    return Offset(
+      r.nextBool() ? r.nextDouble() : -r.nextDouble(),
+      r.nextBool() ? r.nextDouble() : -r.nextDouble(),
+    );
+  }
 }
 
 Future<Set<String>> getPrefKeys() async {
