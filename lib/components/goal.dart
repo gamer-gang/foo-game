3c44e2b69c723f9aaffcb9949e722ac8f34c8319
wiisportsresort <devdoge1@gmail.com>
2020-06-15 01:03:19 -0400

    empty goal

---
+++ lib/components/goal.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/painting.dart';
+
+import '../game.dart';
+import 'gameobject.dart';
+
+class Goal extends GameObject {
+  Goal.create({MonumentPlatformer game}) : super.create(game);
+
+  void render(Canvas c) {}
+
+  void update(double t) {
+
+  }
+}
