12ceec91666bb17a8e017ab2c81b8e91ecbace58
wiisportsresort <devdoge1@gmail.com>
2020-06-15 01:04:33 -0400

    add empty levels 2-10, make levels list 1-indexed

--- lib/components/levels/index.dart
+++ lib/components/levels/index.dart
@@ -3,5 +3,27 @@
 import '../../game.dart';
 import '../level.dart';
 import 'level1.dart';
+import 'level10.dart';
+import 'level2.dart';
+import 'level3.dart';
+import 'level4.dart';
+import 'level5.dart';
+import 'level6.dart';
+import 'level7.dart';
+import 'level8.dart';
+import 'level9.dart';
 
-List<Level Function(MonumentPlatformer)> levels = [level1];
+List<Level Function(MonumentPlatformer)> levels = [
+  // make level 1 = index 1
+  null,
+  level1,
+  level2,
+  level3,
+  level4,
+  level5,
+  level6,
+  level7,
+  level8,
+  level9,
+  level10,
+];
---
+++ lib/components/levels/level2.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+import '../../game.dart';
+import '../background.dart';
+import '../level.dart';
+
+Level level2(MonumentPlatformer game) {
+  // var platformColor = Color(0xff333333);
+
+  return Level.create(
+    game: game,
+    layers: {
+      Layers.background: [
+        Background.create(
+          game: game,
+          color: Color(0xffffffff),
+        ),
+      ],
+      Layers.foreground: [],
+    },
+  );
+}
